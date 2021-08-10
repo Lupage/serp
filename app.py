62a98f099d920aa3851ae0131fea8e6094e87c1c
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2021-08-10 13:57:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,20 @@ def get_serp_results(keyword_argument):
 	    word_count = len(str(paragraph_list).split())
 	    return word_count
 
+	def get_content(argument):
+	    full_url = argument
+	    page_source = requests.get(full_url).text
+	    soup = BeautifulSoup(page_source, 'lxml')
+	    paragraph_list = [element.text for element in soup.find_all('p')]
+	    content = ''.join(paragraph_list[0:3])
+	    return content
+
 	df = seo.get_serps(keyword_argument)
 	df.columns = ["Page Title", "URL", "Text"]
 	word_count = [get_word_count(element) for element in df["URL"]]
 	df["Word Count <p>"] = word_count
+	content = [get_content(element) for element in df["URL"]]
+	df["Excerpt <p>"] = content
 	return df
 
 st.title("*Google Search Results*")
@@ -28,7 +38,7 @@ with st.form(key='my_form'):
 	submit_button = st.form_submit_button(label='Get SERP Results')
 
 if submit_button:
-	st.header("***SERP Results***")
+	st.header(f"***SERP Results for '{keyword}'***")
 	st.table(get_serp_results(keyword))
 	st.header("***Related Keywords***")
 	st.table(seo.google_autocomplete(keyword, include_expanded=True))
