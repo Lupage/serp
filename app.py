dfd1d350cef273bde812cc7f423e7aa3e09229c6
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2021-08-20 21:26:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import requests
 st.set_page_config(layout="wide", page_title="Google Search Results of a Keyword")
 
 def get_serp_results(keyword_argument):
+	
 	def get_word_count(argument):
 	    full_url = argument
 	    page_source = requests.get(full_url).text
@@ -16,16 +17,9 @@ def get_serp_results(keyword_argument):
 	    word_count = len(str(paragraph_list).split())
 	    return word_count
 
-	def get_content(argument):
-	    full_url = argument
-	    page_source = requests.get(full_url).text
-	    soup = BeautifulSoup(page_source, 'lxml')
-	    paragraph_list = [element.text for element in soup.find_all('p')]
-	    content = ''.join(paragraph_list[0:3])
-	    return content
-
 	df = seo.get_serps(keyword_argument)
 	df.columns = ["Page Title", "URL", "Description From SERP"]
+	del df["Description From SERP"]
 	word_count = [get_word_count(element) for element in df["URL"]]
 	df["Word Count <p>"] = word_count
 	keyword_in_title = [keyword_argument.lower() in element.lower() for element in df["Page Title"]]
@@ -33,8 +27,6 @@ def get_serp_results(keyword_argument):
 	matcher = [SequenceMatcher(None, keyword_argument.lower(), element.lower()).ratio()*100 for element in df["Page Title"]]
 	matcher = [str("{:.2f}".format(element))+"%" for element in matcher]
 	df["How Identical is Page Title to Keyword?"] = matcher
-# 	content = [get_content(element) for element in df["URL"]]
-# 	df["Excerpt <p>"] = content
 	return df
 
 st.title("*Google Search Results*")
